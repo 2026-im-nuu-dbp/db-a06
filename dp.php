d9d2b83a77fcfd11b45eab4a1e7f8912a4b30254
ivan950530-cell <ivan950530@gmail.com>
2026-04-14 11:51:35 +0800

    連接資料庫、註冊、登入

---
+++ dp.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$db = '資料庫名稱';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $pdo->query("SELECT * FROM `資料表名稱`");
+    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("連接失敗: " . $e->getMessage());
+}
