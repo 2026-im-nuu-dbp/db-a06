cd758be82c6d74e74bfafa69ff3f88435a1b86cf
ivan950530-cell <ivan950530@gmail.com>
2026-04-21 09:07:12 +0800

    變更

--- log.php
+++ log.php
@@ -24,35 +24,41 @@ function h(string $value): string
 {
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
+?>
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <title>登入紀錄</title>
+    <link rel="stylesheet" href="recipe-login.css">
+</head>
+<body>
+<div class="container">
+    <h1>登入紀錄</h1>
+    <p><a href="Memo.php">回到備忘錄</a></p>
 
-$rowsHtml = '';
-
-if (!$logs) {
-    $rowsHtml = "<tr><td colspan=\"3\">目前沒有登入紀錄</td></tr>";
-} else {
-    foreach ($logs as $log) {
-        $rowsHtml .= '<tr>'
-            . '<td>' . h((string) ($log['account'] ?? '')) . '</td>'
-            . '<td>' . h((string) ($log['login_time'] ?? '')) . '</td>'
-            . '<td>' . h((string) ($log['status'] ?? '')) . '</td>'
-            . '</tr>';
-    }
-}
-
-$templatePath = __DIR__ . DIRECTORY_SEPARATOR . 'log.html';
-
-if (!is_file($templatePath)) {
-    http_response_code(500);
-    echo 'Template file log.html not found.';
-    exit;
-}
-
-$template = file_get_contents($templatePath);
-
-if ($template === false) {
-    http_response_code(500);
-    echo 'Unable to load template.';
-    exit;
-}
-
-echo str_replace('{{TABLE_ROWS}}', $rowsHtml, $template);
+    <table class="log-table">
+        <thead>
+            <tr>
+                <th>帳號</th>
+                <th>時間</th>
+                <th>結果</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php if (!$logs): ?>
+            <tr><td colspan="3">目前沒有登入紀錄</td></tr>
+        <?php else: ?>
+            <?php foreach ($logs as $log): ?>
+                <tr>
+                    <td><?= h((string) ($log['account'] ?? '')) ?></td>
+                    <td><?= h((string) ($log['login_time'] ?? '')) ?></td>
+                    <td><?= h((string) ($log['status'] ?? '')) ?></td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
